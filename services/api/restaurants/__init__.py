80930120d014262bdc25d19258e6f1c8de9a47b5
Jeff Katzy <jeffreyerickatz@gmail.com>
2023-06-13 13:48:58 -0400

    update for lab

--- services/api/restaurants/__init__.py
+++ services/api/restaurants/__init__.py
@@ -4,11 +4,4 @@ from postgres import Postgres
 
 app = Flask(__name__)
 
-DATABASE_URL = os.environ.get('DATABASE_URL')
-connection = Postgres(DATABASE_URL)
-
-
-@app.route('/locations')
-def locations():
-    locations = connection.all('SELECT * FROM locations;')
-    return jsonify({'locations': locations})
+# add flask app here
--- services/frontend/src/index.py
+++ services/frontend/src/index.py
@@ -1,13 +1,8 @@
 import streamlit as st
 import requests
-url = 'http://api:5000/locations'
-response = requests.get(url)
-locations = response.json()
+
 
 st.write("""
 # My first app
 Hello *world!*
 """)
-
-for location in locations['locations']:
-    st.write(location[1])
